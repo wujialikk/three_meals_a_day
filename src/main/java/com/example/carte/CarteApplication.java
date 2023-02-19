f6a3f805eab1f46d969c1c71e406ada000e14bfd
wujialikk <1224750891@qq.com>
2023-02-19 22:02:05 +0800

    一日三餐项目初始化

--- src/main/java/com/example/carte/CarteApplication.java
+++ src/main/java/com/example/carte/CarteApplication.java
@@ -1,9 +1,11 @@
 package com.example.carte;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.carte.mapper")
 public class CarteApplication {
 
     public static void main(String[] args) {
